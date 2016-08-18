da85ae3b778d3fbe744b5f38fefe6117254a7fb8
ShouheiHayashi <ShouheiHayashi@users.noreply.github.com>
2016-08-18 17:34:51 +0800

    Update main.js

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ define(function (require, exports, module) {
                 var Dialogs = brackets.getModule('widgets/Dialogs');
                 Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_ERROR, "", "Unable to transform ES6 and JSX files into ES5, see the debug logs for details.");
                 brackets.fs.writeFile(
-                    file.parentPath + file.name + '.js',
+                    file.parentPath + file.name + '.compiled.js',
                     '',
                     'utf8'
                 );
